101c38b9a7ad5028340c344a89459358bdf0853a
Matt Tremper <matthewt89@gmail.com>
2018-04-30 12:32:28 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,372 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"regexp"
+	"time"
+	"log"
+	
+	"github.com/bwmarrin/discordgo"
+)
+
+type PlanetsideCharacter struct {
+	CharacterId string `json:"id"`
+	Name string `json:"name"`
+	LastSaved string `json:"lastSaved"`
+	FactionId int `json:"factionId"`
+	FactionName string `json:"factionName"`
+	FactionImageId int `json:"factionImageId"`
+	BattleRank int `json:"battleRank"`
+	OutfitAlias string `json:"outfitAlias"`
+	OutfitName string `json:"outfitName"`
+	Kills int `json:"kills"`
+	Deaths int `json:"deaths"`
+	PlayTime int `json:"playTime"`
+	Score int `json:"score"`
+	KillDeathRatio float32 `json:"killDeathRatio"`
+	HeadshotRatio float32 `json:"headshotRatio"`
+	KillsPerHour float32 `json:"killsPerHour"`
+	SiegeLevel float32 `json:"siegeLevel"`
+}
+
+type PlanetsideCharacterWeapon struct {
+	ItemId int `json:"itemId"`
+	WeaponName string `json:"weaponName"`
+	WeaponImageId int `json:"weaponImageId"`
+	Kills int `json:"kills"`
+	Deaths int `json:"deaths"`
+	PlayTime int `json:"playTime"`
+	Score int `json:"score"`
+	Headshots int `json:"headshots"`
+	KillDeathRatio float32 `json:"killDeathRatio"`
+	HeadshotRatio float32 `json:"headshotRatio"`
+	KillsPerHour float32 `json:"killsPerHour"`
+	Accuracy float32 `json:"accuracy"`
+	KillDeathRatioGrade string `json:"killDeathRatioGrade"`
+	HeadshotRatioGrade string `json:"headshotRatioGrade"`
+	KillsPerHourGrade string `json:"killsPerHourGrade"`
+	AccuracyGrade string `json:"accuracyGrade"`
+}
+
+type PlanetsideOutfit struct {
+	Name string `json:"name"`
+	Alias string `json:"alias"`
+	FactionName string `json:"factionName"`
+	FactionImageId int `json:"factionImageId"`
+	WorldName string `json:"worldName"`
+	LeaderName string `json:"leaderName"`
+	MemberCount int `json:"memberCount"`
+	Activity7Days int `json:"activity7Days"`
+	Activity30Days int `json:"activity30Days"`
+	Activity90Days int `json:"activity90Days"`
+}
+
+func init() {
+	flag.StringVar(&token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+var token string
+var buffer = make([][]byte, 0)
+
+func main() {
+	if token == "" {
+		fmt.Println("No token provided. Please run: mutterblack -t <bot token>")
+		return
+	}
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	dg.AddHandler(ready)
+	dg.AddHandler(messageCreate)
+
+	// Open the websocket and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Mutterblack is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+	
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.HasPrefix(m.Content, "!void") {
+		pat, _ := regexp.Compile(`!void (.*?) (.*)`)
+		args := pat.FindAllStringSubmatch(m.Content, -1)[0];
+
+		var serviceGroup = args[1]
+		var serviceArgs = strings.Split(args[2], " ")
+
+		if serviceGroup == "ps2character" {
+			handlePlanetsideCharacter(s, m.ChannelID, serviceArgs)
+		}
+
+		if serviceGroup == "ps2outfit" {
+			handlePlanetsideOutfit(s, m.ChannelID, serviceArgs)
+		}
+	}
+}
+
+func handlePlanetsideCharacter(s *discordgo.Session, channelId string, args []string) {
+	if len(args) == 1 {
+		messagePlanetsideCharacter(s, channelId, args[0])
+	} else if (len(args) == 2) {
+		messagePlanetsideCharacterWeapon(s, channelId, args[0], args[1])
+	}
+}
+
+func messagePlanetsideCharacter(s *discordgo.Session, channelId string, characterName string) {
+	values := map[string]string{"characterName": characterName}
+	content, _ := json.Marshal(values)
+	resp, err := http.Post("http://mutterblack/command/planetside2/character", "application/json", bytes.NewBuffer(content))
+	if err != nil {
+		s.ChannelMessageSend(channelId, "Failed to retrieve data :(")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var character PlanetsideCharacter
+	json.Unmarshal(body, &character)
+
+	lastSaved, _ := time.Parse(time.RFC3339, character.LastSaved)
+
+	embed := &discordgo.MessageEmbed{
+		Author: &discordgo.MessageEmbedAuthor{
+			Name: character.Name,
+		},
+		Color: 0x070707,
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+        	URL: fmt.Sprintf("http://census.daybreakgames.com/files/ps2/images/static/%d.png", character.FactionImageId),
+    	},
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{
+				Name: "Last Seen",
+				Value: fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d UTC", lastSaved.Year(), lastSaved.Month(), lastSaved.Day(), lastSaved.Hour(), lastSaved.Minute(), lastSaved.Second()),
+				Inline: false,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Battle Rank",
+				Value: fmt.Sprintf("%d", character.BattleRank),
+				Inline: false,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Outfit",
+				Value: "[" + character.OutfitAlias + "]" + character.OutfitName,
+				Inline: false,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Play Time",
+				Value: fmt.Sprintf("%d Hours", character.PlayTime / 3600),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "KDR",
+				Value: fmt.Sprintf("%0.2f", character.KillDeathRatio),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "HSR",
+				Value: fmt.Sprintf("%0.2f%%", character.HeadshotRatio * 100),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "KpH",
+				Value: fmt.Sprintf("%0.2f", character.KillsPerHour),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Siege Level",
+				Value: fmt.Sprintf("%0.1f", character.SiegeLevel),
+				Inline: true,
+        	},
+		},
+	}
+
+	s.ChannelMessageSendEmbed(channelId, embed)
+}
+
+func messagePlanetsideCharacterWeapon(s *discordgo.Session, channelId string, characterName string, weaponName string) {
+	values := map[string]string{"characterName": characterName, "weaponName": weaponName}
+	content, _ := json.Marshal(values)
+	resp, err := http.Post("http://mutterblack/command/planetside2/character-weapon", "application/json", bytes.NewBuffer(content))
+	if err != nil {
+		s.ChannelMessageSend(channelId, "Failed to retrieve data :(")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)	
+
+	var weapon PlanetsideCharacterWeapon
+	json.Unmarshal(body, &weapon)
+	
+	embed := &discordgo.MessageEmbed{
+		Author: &discordgo.MessageEmbedAuthor{
+			Name: characterName + " [" + weapon.WeaponName + "]",
+		},
+		Color: 0x070707,
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+        	URL: fmt.Sprintf("http://census.daybreakgames.com/files/ps2/images/static/%d.png", weapon.WeaponImageId),
+    	},
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{
+				Name: "Kills",
+				Value: fmt.Sprintf("%d", weapon.Kills),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Deaths",
+				Value: fmt.Sprintf("%d", weapon.Deaths),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Play Time",
+				Value: fmt.Sprintf("%d Minutes", weapon.PlayTime / 60),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Score",
+				Value: fmt.Sprintf("%d", weapon.Score),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "KpH",
+				Value: fmt.Sprintf("%0.2f", weapon.KillsPerHour),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Δ",
+				Value: weapon.KillsPerHourGrade,
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "KDR",
+				Value: fmt.Sprintf("%0.2f", weapon.KillDeathRatio),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Δ",
+				Value: weapon.KillDeathRatioGrade,
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "HSR",
+				Value: fmt.Sprintf("%0.2f%%", weapon.HeadshotRatio * 100),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Δ",
+				Value: weapon.HeadshotRatioGrade,
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Accuracy",
+				Value: fmt.Sprintf("%0.2f%%", weapon.Accuracy * 100),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Δ",
+				Value: weapon.AccuracyGrade,
+				Inline: true,
+        	},
+		},
+	}
+
+	message, err := s.ChannelMessageSendEmbed(channelId, embed)
+	_ = message
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func handlePlanetsideOutfit(s *discordgo.Session, channelId string, args []string) {
+	if len(args) == 1 {
+		messagePlanetsideOutfit(s, channelId, args[0])
+	}
+}
+
+func messagePlanetsideOutfit(s *discordgo.Session, channelId string, outfitAlias string) {
+	values := map[string]string{"outfitAlias": outfitAlias}
+	content, _ := json.Marshal(values)
+	resp, err := http.Post("http://mutterblack/command/planetside2/outfit", "application/json", bytes.NewBuffer(content))
+	if err != nil {
+		s.ChannelMessageSend(channelId, "Failed to retrieve data :(")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var outfit PlanetsideOutfit
+	json.Unmarshal(body, &outfit)
+
+	embed := &discordgo.MessageEmbed{
+		Author: &discordgo.MessageEmbedAuthor{
+			Name: "[" + outfit.Alias + "] " + outfit.Name,
+		},
+		Color: 0x070707,
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+        	URL: fmt.Sprintf("http://census.daybreakgames.com/files/ps2/images/static/%d.png", outfit.FactionImageId),
+    	},
+		Fields: []*discordgo.MessageEmbedField{
+			&discordgo.MessageEmbedField{
+				Name: "Server",
+				Value: outfit.WorldName,
+				Inline: false,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Leader",
+				Value: outfit.LeaderName,
+				Inline: false,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Member Count",
+				Value: fmt.Sprintf("%d", outfit.MemberCount),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Activity 7 Days",
+				Value: fmt.Sprintf("%d", outfit.Activity7Days),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Activity 30 Days",
+				Value: fmt.Sprintf("%d", outfit.Activity30Days),
+				Inline: true,
+        	},
+			&discordgo.MessageEmbedField{
+				Name: "Activity 90 Days",
+				Value: fmt.Sprintf("%d", outfit.Activity90Days),
+				Inline: true,
+        	},
+		},
+	}
+
+	s.ChannelMessageSendEmbed(channelId, embed)
+}
