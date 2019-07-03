db2e09cd4bd24fc11d44fcb5d225878b59549db6
Lampjaw <matthewt89@gmail.com>
2019-07-03 14:31:14 -0400

    Fix nil string

--- bot.go
+++ bot.go
@@ -85,7 +85,7 @@ func findCommandMatch(b *Bot, plugin Plugin, message Message) {
 				log.Printf("<%s> %s: %s\n", message.Channel(), message.UserName(), message.Message())
 
 				if commandDefinition.Arguments == nil {
-					commandDefinition.Callback(b, b.Client, message, nil, nil)
+					commandDefinition.Callback(b, b.Client, message, nil, "")
 					return
 				}
 
